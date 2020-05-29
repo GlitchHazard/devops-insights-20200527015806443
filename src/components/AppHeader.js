787ca55b81b3ab88ad0e460d06128af0f28b901b
Zac Lawson <zacharylawson01@gmail.com>
2020-05-29 05:03:32 +0000

    test commit part 2

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AppHeader() {
 
-    return <div>
+    return <div id="head">
         <p className="p-3 text-light h3 bg-primary">Current weather by zip code. Hello COMPX341-20A</p>
     </div>
 }
